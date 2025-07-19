8bc1f588655ebe16471cb86b28db634533bfb015
SanjeevRai07 <sr1415014@gmail.com>
2025-07-19 21:06:00 +0530

    Java projects

---
+++ Labprogram.java
@@ -0,0 +1,8 @@
+public class Labprogram {
+    public static void main(String[] args) {
+        double randomNO=Math.random();
+        int random_number =(int)(Math.random()*101);
+        System.out.println("RAndom Double:" +  random_number);
+        System.out.println("Random Integer:" + randomNO);
+    }
+}
