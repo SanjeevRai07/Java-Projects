1bc8f05e564db187b9cc1f91b77ae8b41e9201e8
SanjeevRai07 <146112569+SanjeevRai07@users.noreply.github.com>
2025-07-20 20:12:49 +0530

    Add files via upload

---
+++ star1.java
@@ -0,0 +1,13 @@
+public class star1 {
+    public static void main(String[] agrs){
+        int n = 4;
+        for(int i = 1; i<=n; i++){
+            for(int j =1 ;j<=n; j++){
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
