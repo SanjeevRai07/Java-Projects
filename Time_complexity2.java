7fa376c5db4a1d62239a791606a5b070bf84ab47
SanjeevRai07 <146112569+SanjeevRai07@users.noreply.github.com>
2025-08-02 23:41:40 +0530

    Add files via upload

---
+++ Time_complexity2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Time_complexity2{
+    public class time_complexity {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i =0; i<n; i++){
+            for(int j=0;j<n;j++){
+                 System.out.println("Hello boss");
+
+            }
+           
+        }
+    }
+
+    }
+}
