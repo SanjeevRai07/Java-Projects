6fb4c6f0ea8cf63c986a2b906a75b58c2b14d858
SanjeevRai07 <146112569+SanjeevRai07@users.noreply.github.com>
2025-08-08 22:16:31 +0530

    Add files via upload

---
+++ Prime_check.java
@@ -0,0 +1,14 @@
+public class Prime_check {
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 2789;
+        System.out.println(num + (isPrime(num) ? " is Prime" : " is Not Prime"));
+    }
+}
