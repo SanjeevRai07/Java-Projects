c1ff4ccd708e7d866c12fcba24867f23a6fd800c
Sanjeev Rai <146112569+SanjeevRai07@users.noreply.github.com>
2025-08-15 19:22:30 +0530

    Add files via upload

---
+++ Triangle_Array.java
@@ -0,0 +1,12 @@
+public class Triangle_Array {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
