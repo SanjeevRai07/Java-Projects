47b00a29c9cc64aea96f660f829494389b2860aa
Sanjeev Rai <146112569+SanjeevRai07@users.noreply.github.com>
2025-08-17 22:31:54 +0530

    Add files via upload

---
+++ Cross_mix_pattern.java
@@ -0,0 +1,17 @@
+public class Cross_mix_pattern {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    System.out.print(arr[j] + " ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
