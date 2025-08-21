13fa9c82b48747e07da16de30b95233bce93aebf
Sanjeev Rai <146112569+SanjeevRai07@users.noreply.github.com>
2025-08-21 22:26:14 +0530

    Add files via upload

---
+++ Factorial_rec.java
@@ -0,0 +1,12 @@
+public class Factorial_rec {
+    static int factorial(int n) {
+        if (n == 0 || n == 1) 
+            return 1;
+        else 
+            return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5: " + factorial(5));
+    }
+}
